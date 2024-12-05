6a71b270c6138b574f39f88007968b56f747eb52
HelloBeHappy93 <155662316+HelloBeHappy93@users.noreply.github.com>
2024-12-05 16:04:36 +0800

    Create main1.c

---
+++ EX3/main1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 10;
+
+    if(i>0&&(i&(i-1))==0)
+    {
+        printf("true\n");
+    }
+    else
+    {
+        printf("false\n");
+    }
+    return 0;
+}
