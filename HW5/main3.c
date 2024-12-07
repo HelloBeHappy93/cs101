c32ac454ccebd07b1c7cd5547a50795223fb8a34
HelloBeHappy93 <155662316+HelloBeHappy93@users.noreply.github.com>
2024-12-07 08:02:51 +0800

    Create main3.c

---
+++ HW5/main3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 1; i < 10;i++)
+    {
+        printf("1*%d=%d 2*%d=%d 3*%d=%d 4*%d=%d 5*%d=%d 6*%d=%d 7*%d=%d 8*%d=%d 9*%d=%d\n", i, i * 1, i, i * 2, i, i * 3, i, i * 4, i, i * 5, i, i * 6, i, i * 7, i, i * 8, i, i * 9);
+    }
+    return 0;
+}
