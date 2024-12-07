fa64e5999a983f47dc927f7b4de6deb4441a19c1
HelloBeHappy93 <155662316+HelloBeHappy93@users.noreply.github.com>
2024-12-07 08:03:13 +0800

    Create main4.c

---
+++ HW5/main4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int number,ones,thousands,changeN;
+    number = 12345;
+    ones = number % 10;
+    thousands = (number / 1000) % 10;
+    changeN = number - (thousands * 1000 + ones) + (ones * 1000 + thousands);
+    printf("%i", changeN);
+    return 0;
+}
